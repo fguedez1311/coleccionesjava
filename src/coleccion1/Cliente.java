8c8e419fc0560840f29bce337827695a4a9ba838
fguedez <fguedez1311@gmail.com>
2023-09-28 23:58:34 -0400

    Primera coleccion

---
+++ src/coleccion1/Cliente.java
@@ -0,0 +1,36 @@
+package coleccion1;
+
+public class Cliente {
+    private String nombre;
+    private String n_cuenta;
+    private double saldo;
+    public Cliente(String nombre, String n_cuenta, double saldo) {
+        this.nombre = nombre;
+        this.n_cuenta = n_cuenta;
+        this.saldo = saldo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getN_cuenta() {
+        return n_cuenta;
+    }
+    public void setN_cuenta(String n_cuenta) {
+        this.n_cuenta = n_cuenta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    @Override
+    public String toString() {
+        return "Cliente [nombre=" + nombre + ", n_cuenta=" + n_cuenta + ", saldo=" + saldo + "]";
+    }
+    
+    
+}
---
+++ src/coleccion1/CuentasUsuarios.java
@@ -0,0 +1,26 @@
+package coleccion1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CuentasUsuarios {
+    public static void main(String[] args) {
+        Cliente cl1 =new Cliente("Antonio Banderas", "0001", 20000);
+        Cliente cl2 =new Cliente("Rafael Nadal", "0002", 25000);
+        Cliente cl3 =new Cliente("Penelope Cruz", "0003", 300000);
+        Cliente cl4 =new Cliente("Julio Iglesia", "0004", 500000);
+
+        Set<Cliente> clientesBanco=new HashSet<>();
+        clientesBanco.add(cl1);
+        clientesBanco.add(cl2);
+        clientesBanco.add(cl3);
+        clientesBanco.add(cl4);
+
+
+        for(Cliente cliente:clientesBanco){
+            System.out.println(cliente);
+        }
+
+
+    }
+}
